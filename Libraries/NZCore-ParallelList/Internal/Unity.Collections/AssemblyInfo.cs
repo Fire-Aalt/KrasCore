a73e1cf47db69af7455a1cb160c1e813ca49cd24
Maksim Krasutski <krasutskimaksim@gmail.com>
2025-06-22 13:01:43 +0200

    Mosaic 1.2.0

--- Libraries/NZCore-ParallelList/Internal/Unity.Collections/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// <copyright project="Unity.Collections" file="AssemblyInfo.cs" version="1.0.0">
-// Copyright © 2024 Thomas Enzenebner. All rights reserved.
-// </copyright>
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("KrasCore.NZCore")]
