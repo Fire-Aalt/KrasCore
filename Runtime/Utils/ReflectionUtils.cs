bba24a57afd805c05f1ca958acdae3ea6b294684
Maksim Krasutski <krasutskimaksim@gmail.com>
2025-06-26 15:23:39 +0200

    Add position field binding + refactor

--- Runtime/Utils/ReflectionUtils.cs
+++ Runtime/Utils/ReflectionUtils.cs
@@ -32,5 +32,40 @@ namespace KrasCore
             
             return (T)method.Invoke(targetObject, parameters);
         }
+        
+        public static bool TryCallMethod(object targetObject, string methodName, params object[] parameters)
+        {
+            if (string.IsNullOrEmpty(methodName))
+            {
+                return false;
+            }
+            
+            var method = targetObject.GetType().GetMethod(methodName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            
+            if (method == null) 
+                throw new Exception($"Method '{methodName}' not found on target object '{targetObject}'.");
+            
+            method.Invoke(targetObject, parameters);
+            return true;
+        }
+        
+        public static bool TryCallMethodReturn<T>(object targetObject, string methodName, out T result, params object[] parameters)
+        {
+            if (string.IsNullOrEmpty(methodName))
+            {
+                result = default;
+                return false;
+            }
+            
+            var method = targetObject.GetType().GetMethod(methodName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            
+            if (method == null) 
+                throw new Exception($"Method '{methodName}' not found on target object '{targetObject}'.");
+            
+            result = (T)method.Invoke(targetObject, parameters);
+            return true;
+        }
     }
 }
