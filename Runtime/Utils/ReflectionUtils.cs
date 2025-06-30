7e198aeedfe76e32cc7a8a83971d7f89e5239a25
Maksim Krasutski <krasutskimaksim@gmail.com>
2025-06-30 16:00:05 +0200

    Return TypeName based VFXDataType access

--- Runtime/Utils/ReflectionUtils.cs
+++ Runtime/Utils/ReflectionUtils.cs
@@ -7,65 +7,73 @@ namespace KrasCore
     {
         public static void CallMethod(object targetObject, string methodName, params object[] parameters)
         {
-            if (string.IsNullOrEmpty(methodName)) 
-                throw new Exception($"Method name is null or empty");
-            
-            var method = targetObject.GetType().GetMethod(methodName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            
-            if (method == null) 
-                throw new Exception($"Method '{methodName}' not found on target object '{targetObject}'.");
-            
+            var method = GetCallMethod(targetObject, methodName);
+
             method.Invoke(targetObject, parameters);
         }
-        
+
         public static T CallMethodReturn<T>(object targetObject, string methodName, params object[] parameters)
         {
-            if (string.IsNullOrEmpty(methodName)) 
-                throw new Exception($"Method name is null or empty");
-            
-            var method = targetObject.GetType().GetMethod(methodName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            
-            if (method == null) 
-                throw new Exception($"Method '{methodName}' not found on target object '{targetObject}'.");
+            var method = GetCallMethod(targetObject, methodName);
             
             return (T)method.Invoke(targetObject, parameters);
         }
         
+        public static TReturn CallMethodOut<TReturn, TOut>(object targetObject, string methodName, out TOut result, params object[] parameters)
+        {
+            var method = GetCallMethod(targetObject, methodName);
+            
+            var newParams = new object[parameters != null ? parameters.Length + 1 : 1];
+            
+            var ret = (TReturn)method.Invoke(targetObject, newParams);
+            result = (TOut)newParams[^1];
+            return ret;
+        }
+        
         public static bool TryCallMethod(object targetObject, string methodName, params object[] parameters)
         {
-            if (string.IsNullOrEmpty(methodName))
-            {
-                return false;
-            }
-            
-            var method = targetObject.GetType().GetMethod(methodName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            
-            if (method == null) 
-                throw new Exception($"Method '{methodName}' not found on target object '{targetObject}'.");
-            
+            if (!TryGetCallMethod(targetObject, methodName, out var method)) return false;
+
             method.Invoke(targetObject, parameters);
             return true;
         }
-        
+
         public static bool TryCallMethodReturn<T>(object targetObject, string methodName, out T result, params object[] parameters)
         {
-            if (string.IsNullOrEmpty(methodName))
-            {
-                result = default;
-                return false;
-            }
-            
-            var method = targetObject.GetType().GetMethod(methodName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            
-            if (method == null) 
-                throw new Exception($"Method '{methodName}' not found on target object '{targetObject}'.");
+            result = default;
+            if (!TryGetCallMethod(targetObject, methodName, out var method)) return false;
             
             result = (T)method.Invoke(targetObject, parameters);
             return true;
+        }
+        
+        private static MethodInfo GetCallMethod(object targetObject, string methodName)
+        {
+            if (string.IsNullOrEmpty(methodName)) 
+                throw new Exception($"Method name is null or empty");
+            return GetMethodSignature(targetObject, methodName);
+        }
+
+        private static bool TryGetCallMethod(object targetObject, string methodName, out MethodInfo method)
+        {
+            if (string.IsNullOrEmpty(methodName))
+            {
+                method = null;
+                return false;
+            }
+            
+            method = GetMethodSignature(targetObject, methodName);
+            return true;
+        }
+        
+        private static MethodInfo GetMethodSignature(object targetObject, string methodName)
+        {
+            var method = targetObject.GetType().GetMethod(methodName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (method == null) 
+                throw new Exception($"Method '{methodName}' not found on target object '{targetObject}'.");
+            return method;
         }
     }
 }
