a6ebeeec2e13a5c5466201c6e03e996d93fd3dbe
Maksim Krasutski <krasutskimaksim@gmail.com>
2025-08-26 16:41:35 +0200

    Fix VFX dependency chain

--- Runtime/Extensions/Collections/NativeListExtensions.cs
+++ Runtime/Extensions/Collections/NativeListExtensions.cs
@@ -1,31 +1,19 @@
-using System.Threading;
 using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
 using Unity.Mathematics;
 
 namespace KrasCore
 {
     public static class NativeListExtensions
     {
-        public static unsafe void RemoveAtSwapBack<T>(this UnsafeList<T>.ParallelWriter writer, int index) where T : unmanaged
-        {
-            var newLength = Interlocked.Decrement(ref writer.ListData->m_length);
-            
-            if (index < newLength)
-            {
-                writer.ListData->Ptr[index] = writer.ListData->Ptr[newLength];
-            }
-        }
-        
         public static void Remove<T>(this NativeList<T> list, T element)
             where T : unmanaged
         {
             for (int i = list.Length - 1; i >= 0; i--)
             {
-                if (list[i].GetHashCode() != element.GetHashCode())
-                    continue;
-
-                list.RemoveAt(i);
+                if (list[i].GetHashCode() == element.GetHashCode() && list[i].Equals(element))
+                {
+                    list.RemoveAt(i);
+                }
             }
         }
         
