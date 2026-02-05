8cee56d7dd34be839927610b9041628fbf168d22
Maksim Krasutski <krasutskimaksim@gmail.com>
2026-02-05 12:33:35 +0100

    Improve UX

--- Editor/Tools/Artifice_EditorWindow.cs
+++ Editor/Tools/Artifice_EditorWindow.cs
@@ -1,13 +1,26 @@
+using System;
 using ArtificeToolkit.Editor;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace KrasCore.Editor
 {
-    public class Artifice_EditorWindow :  EditorWindow
+    public class Artifice_EditorWindow : EditorWindow
     {
         public virtual void CreateGUI()
         {
             rootVisualElement.Add(new ArtificeDrawer().CreateInspectorGUI(new SerializedObject(this)));
+        }
+        
+        protected void RegisterEnableButtonIf(string buttonName, Func<bool> showCondition)
+        {
+            var modifyButton = rootVisualElement.Q<Button>(buttonName);
+            modifyButton.schedule.Execute(UpdateButtonVisibility).Every(250);
+            
+            void UpdateButtonVisibility()
+            {
+                modifyButton.style.display = showCondition() ? DisplayStyle.Flex : DisplayStyle.None;
+            }
         }
     }
 }
