f1ac995e31b22437f5cc1b3909e0a7668dde9e75
Maksim Krasutski <krasutskimaksim@gmail.com>
2026-04-11 14:33:28 +0200

    Refactor

--- Runtime/Utils/Groups/LateInitializationSystemGroup.cs
+++ Runtime/Utils/Groups/LateInitializationSystemGroup.cs
@@ -1,11 +1,11 @@
 using BovineLabs.Core.Groups;
+using BovineLabs.Core.LifeCycle;
 using Unity.Entities;
 
 namespace KrasCore
 {
     [WorldSystemFilter(WorldSystemFilterFlags.Default | WorldSystemFilterFlags.Editor)]
-    [UpdateInGroup(typeof(BeginSimulationSystemGroup))]
-    //    [UpdateInGroup(typeof(BeginSimulationSystemGroup), OrderFirst = true)]
-    //    [UpdateAfter(typeof(SceneInitializeSystem))]
+    [UpdateInGroup(typeof(BeginSimulationSystemGroup), OrderFirst = true)]
+    [UpdateAfter(typeof(SceneInitializeSystem))]
     public partial class LateInitializationSystemGroup : ComponentSystemGroup { }
 }
