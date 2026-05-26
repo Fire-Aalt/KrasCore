43457110ccb052dada1291cbf0b9fc7593705585
Maksim Krasutski <krasutskimaksim@gmail.com>
2026-05-26 12:21:03 +0200

    Switch to using Ref<>

--- Runtime/Collections/Pooled/ListPool.cs
+++ Runtime/Collections/Pooled/ListPool.cs
@@ -7,7 +7,7 @@ using Unity.Jobs.LowLevel.Unsafe;
 namespace KrasCore.Collections.Pooled
 {
     /// <summary>
-    /// A pooled wrapper around NativeList that reuses allocated memory across instances to reduce allocation pressure.
+    /// A pooled wrapper around UnsafeList that reuses allocated memory across instances to reduce allocation pressure.
     /// Uses thread-local pools to avoid contention in multi-threaded scenarios.
     /// </summary>
     /// <typeparam name="T">The unmanaged element type.</typeparam>
@@ -27,6 +27,23 @@ namespace KrasCore.Collections.Pooled
             return default(PooledNativeList<T>).Create(minCapacity);
         }
     }
+
+    /// <summary>
+    /// A pool for UnsafeList instances that share backing storage with <see cref="NativeListPool{T}" />.
+    /// </summary>
+    /// <typeparam name="T">The unmanaged element type.</typeparam>
+    public struct UnsafeListPool<T>
+        where T : unmanaged
+    {
+        /// <summary>
+        /// Creates a new PooledUnsafeList instance, either from the thread-local pool or by allocating a new one.
+        /// </summary>
+        /// <returns>A PooledUnsafeList instance ready for use.</returns>
+        public static PooledUnsafeList<T> Rent(int minCapacity = 0)
+        {
+            return default(PooledUnsafeList<T>).Create(minCapacity);
+        }
+    }
     
     /// <summary>
     /// A pooled wrapper around NativeList that reuses allocated memory across instances to reduce allocation pressure.
@@ -36,62 +53,28 @@ namespace KrasCore.Collections.Pooled
     public unsafe struct PooledUnsafeList<T> : IDisposable
         where T : unmanaged
     {
-        private UnsafeList<T> _list;
+        private Ref<UnsafeList<T>> _list;
 
         /// <summary>
-        /// Gets the underlying NativeList instance.
+        /// Gets the underlying UnsafeList instance.
         /// </summary>
-        /// <value>The wrapped NativeList that can be used for all list operations.</value>
-        public UnsafeList<T> List => _list;
+        /// <value>The wrapped UnsafeList that can be used for all list operations.</value>
+        public ref UnsafeList<T> List => ref _list.Value;
         
         internal PooledUnsafeList<T> Create(int minCapacity)
         {
-            ref var data = ref PooledNativeList.Pool.Data;
-
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-            if (!data.IsCreated)
-            {
-                throw new InvalidOperationException("PooledNativeList pool not initialized.");
-            }
-#endif
-
-            ref var lp = ref PooledNativeList.Pool.Data.GetThreadList();
-            if (lp.Length == 0)
-            {
-                // Nothing in the pool, just create a new one
-                _list = new NativeList<T>(minCapacity, data.Allocator);
-            }
-            else
-            {
-                // Pop an existing list out
-                var byteList = lp[^1];
-                lp.RemoveAt(lp.Length - 1);
-
-                _list = UnsafeUtility.As<NativeList<byte>, NativeList<T>>(ref byteList);
-                
-                var capacity = byteList.Capacity / UnsafeUtility.SizeOf<T>();
-                if (capacity < minCapacity)
-                {
-                    _list.Capacity = minCapacity;
-                }
-                else
-                {
-                    _list.m_ListData->m_capacity = capacity;
-                }
-            }
-
+            _list = ListPool.RentUnsafeList<T>(minCapacity);
             return this;
         }
 
-        public UnsafeArray<T> AsArray(int length, NativeArrayOptions options = NativeArrayOptions.ClearMemory) 
+        public UnsafeArray<T> AsUnsafeArray(int length, NativeArrayOptions options = NativeArrayOptions.ClearMemory) 
         {
-            _list.Resize(length, options);
-            return _list.AsArray();
+            List.Resize(length, options);
+            return UnsafeArrayUtility.ConvertExistingDataToUnsafeArray<T>(List.Ptr, List.Length, Allocator.None);
         }
         
-        
         /// <summary>
-        /// Disposes the PooledNativeList and returns the underlying memory to the thread-local pool for reuse.
+        /// Disposes the PooledUnsafeList and returns the underlying memory to the thread-local pool for reuse.
         /// </summary>
         /// <remarks>
         /// This method clears the list contents and converts it back to a byte list for storage in the pool.
@@ -99,31 +82,13 @@ namespace KrasCore.Collections.Pooled
         /// </remarks>
         public void Dispose()
         {
-            if (!_list.IsCreated)
+            if (_list == null)
             {
                 return;
             }
 
-            ref var lp = ref PooledNativeList.Pool.Data.GetThreadList();
-
-            _list.Clear();
-
-            // Convert back to a byte list
-            ref var byteList = ref UnsafeUtility.As<NativeList<T>, NativeList<byte>>(ref _list);
-            byteList.m_ListData->m_capacity = _list.Capacity * UnsafeUtility.SizeOf<T>();
-
-            // Only add back to pool if we haven't exceeded the max size for lists and max allocation size
-            if (lp.Length < PooledNativeList.MAX_POOL_SIZE_PER_THREAD && byteList.Capacity < PooledNativeList.MAX_BYTES_PER_LIST)
-            {
-                lp.Add(byteList);
-            }
-            else
-            {
-                // Pool is full, dispose the list instead
-                byteList.Dispose();
-            }
-
-            _list = default;
+            ListPool.ReturnUnsafeList(_list);
+            _list = null;
         }
     }
     
@@ -138,10 +103,6 @@ namespace KrasCore.Collections.Pooled
     {
         private NativeList<T> _list;
 
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-        private AtomicSafetyHandle _oldHandle;
-#endif
-
         /// <summary>
         /// Gets the underlying NativeList instance.
         /// </summary>
@@ -150,44 +111,13 @@ namespace KrasCore.Collections.Pooled
         
         internal PooledNativeList<T> Create(int minCapacity)
         {
-            ref var data = ref PooledNativeList.Pool.Data;
+            _list = default;
+            _list.m_ListData = ListPool.RentUnsafeList<T>(minCapacity);
 
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
-            if (!data.IsCreated)
-            {
-                throw new InvalidOperationException("PooledNativeList pool not initialized.");
-            }
-#endif
-
-            ref var lp = ref PooledNativeList.Pool.Data.GetThreadList();
-            if (lp.Length == 0)
-            {
-                // Nothing in the pool, just create a new one
-                _list = new NativeList<T>(minCapacity, data.Allocator);
-            }
-            else
-            {
-                // Pop an existing list out
-                var byteList = lp[^1];
-                lp.RemoveAt(lp.Length - 1);
-                
-                _list = UnsafeUtility.As<NativeList<byte>, NativeList<T>>(ref byteList);
-                
-                var capacity = byteList.Capacity / UnsafeUtility.SizeOf<T>();
-                if (capacity < minCapacity)
-                {
-                    _list.Capacity = minCapacity;
-                }
-                else
-                {
-                    _list.m_ListData->m_capacity = capacity;
-                }
-            }
-
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-            // Replace our safety as it's not valid within the job as we've stored these inside another container so can't be injected
-            _oldHandle = _list.m_Safety;
-            _list.m_Safety = AtomicSafetyHandle.Create();
+            _list.m_Safety = CollectionHelper.CreateSafetyHandle(ListPool.Pool.Data.Allocator);
+            CollectionHelper.SetStaticSafetyId<NativeList<T>>(ref _list.m_Safety, ref NativeList<T>.s_staticSafetyId.Data);
+            AtomicSafetyHandle.SetBumpSecondaryVersionOnScheduleWrite(_list.m_Safety, true);
 #endif
 
             return this;
@@ -214,47 +144,83 @@ namespace KrasCore.Collections.Pooled
                 return;
             }
 
-            ref var lp = ref PooledNativeList.Pool.Data.GetThreadList();
-
-            _list.Clear();
-
-            // Convert back to a byte list
-            ref var byteList = ref UnsafeUtility.As<NativeList<T>, NativeList<byte>>(ref _list);
-            byteList.m_ListData->m_capacity = _list.Capacity * UnsafeUtility.SizeOf<T>();
-
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
-            // Release the Temp handle
-            AtomicSafetyHandle.CheckDeallocateAndThrow(byteList.m_Safety);
-            AtomicSafetyHandle.Release(byteList.m_Safety);
-            byteList.m_Safety = _oldHandle;
+            AtomicSafetyHandle.CheckDeallocateAndThrow(_list.m_Safety);
+            AtomicSafetyHandle.Release(_list.m_Safety);
 #endif
 
-            // Only add back to pool if we haven't exceeded the max size for lists and max allocation size
-            if (lp.Length < PooledNativeList.MAX_POOL_SIZE_PER_THREAD && byteList.Capacity < PooledNativeList.MAX_BYTES_PER_LIST)
-            {
-                lp.Add(byteList);
-            }
-            else
-            {
-                // Pool is full, dispose the list instead
-                byteList.Dispose();
-            }
-
+            ListPool.ReturnUnsafeList(new Ref<UnsafeList<T>>(_list.m_ListData));
             _list = default;
-
-#if ENABLE_UNITY_COLLECTIONS_CHECKS
-            _oldHandle = default;
-#endif
         }
     }
 
-    internal static class PooledNativeList
+    internal static class ListPool
     {
         // About 1MB per thread
-        internal const int MAX_POOL_SIZE_PER_THREAD = 16;
-        internal const int MAX_BYTES_PER_LIST = 64 * 1024;
+        private const int MAX_POOL_SIZE_PER_THREAD = 16;
+        private const int MAX_BYTES_PER_LIST = 64 * 1024;
 
         internal static readonly SharedStatic<Data> Pool = SharedStatic<Data>.GetOrCreate<Data>();
+
+        internal static unsafe Ref<UnsafeList<T>> RentUnsafeList<T>(int minCapacity)
+            where T : unmanaged
+        {
+            ref var data = ref Pool.Data;
+
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            if (!data.IsCreated)
+            {
+                throw new InvalidOperationException("PooledNativeList pool not initialized.");
+            }
+#endif
+
+            ref var lp = ref data.GetThreadList();
+            if (lp.Length == 0)
+            {
+                var listData = AllocatorManager.Allocate<UnsafeList<T>>(Pool.Data.Allocator);
+                *listData = new UnsafeList<T>(minCapacity, data.Allocator);
+                return new Ref<UnsafeList<T>>(listData);
+            }
+
+            var byteList = lp[^1];
+            lp.RemoveAt(lp.Length - 1);
+            
+            var list = UnsafeUtility.As<Ref<UnsafeList<byte>>, Ref<UnsafeList<T>>>(ref byteList);
+            var capacity = byteList.Value.Capacity / UnsafeUtility.SizeOf<T>();
+            if (capacity < minCapacity)
+            {
+                list.Value.Capacity = minCapacity;
+            }
+            else
+            {
+                list.Value.m_capacity = capacity;
+            }
+
+            list.Value.m_length = 0;
+            return list;
+        }
+        
+        internal static unsafe void ReturnUnsafeList<T>(Ref<UnsafeList<T>> list)
+            where T : unmanaged
+        {
+            ref var refByteList = ref UnsafeUtility.As<Ref<UnsafeList<T>>, Ref<UnsafeList<byte>>>(ref list);
+            ref var byteList = ref refByteList.Value;
+            byteList.Clear();
+
+            byteList.m_capacity = list.Value.Capacity * UnsafeUtility.SizeOf<T>();
+            byteList.m_length = 0;
+
+            ref var lp = ref Pool.Data.GetThreadList();
+            if (lp.Length < MAX_POOL_SIZE_PER_THREAD && byteList.Capacity < MAX_BYTES_PER_LIST)
+            {
+                lp.Add(refByteList);
+            }
+            else
+            {
+                byteList.Dispose();
+                AllocatorManager.Free(Pool.Data.Allocator, refByteList.GetUnsafePtr());
+            }
+        }
 
         /// <summary>
         /// Initializes the global pool data structure used by all PooledNativeList instances.
@@ -288,13 +254,13 @@ namespace KrasCore.Collections.Pooled
                 
                 for (var i = 0; i < _buffer.Length; i++)
                 {
-                    _buffer.GetThreadDataRef(i).ThreadList = new UnsafeList<UnsafeList<byte>>(0, allocator);
+                    _buffer.GetThreadDataRef(i).ThreadList = new UnsafeList<Ref<UnsafeList<byte>>>(0, allocator);
                 }
             }
 
             public readonly bool IsCreated => _buffer.IsCreated;
 
-            public ref UnsafeList<UnsafeList<byte>> GetThreadList()
+            public ref UnsafeList<Ref<UnsafeList<byte>>> GetThreadList()
             {
 #if UNITY_EDITOR
                 UnityEngine.Debug.Assert(JobsUtility.IsExecutingJob || UnityEditorInternal.InternalEditorUtility.CurrentThreadIsMainThread(),
@@ -314,7 +280,7 @@ namespace KrasCore.Collections.Pooled
                 {
                     foreach (var list in threadData.ThreadList)
                     {
-                        list.Dispose();
+                        list.Value.Dispose();
                     }
                     threadData.ThreadList.Dispose();
                 }
@@ -324,7 +290,7 @@ namespace KrasCore.Collections.Pooled
 
         private struct ThreadData
         {
-            public UnsafeList<UnsafeList<byte>> ThreadList;
+            public UnsafeList<Ref<UnsafeList<byte>>> ThreadList;
         }
     }
 }
